e290008e1b4508e2c7291fe9940389356e3a2015
Stardustlv8 <teku-acceledrom-bass@live.com.mx>
2017-02-19 10:54:54 -0600

    Metodo Biseccion primer commit

---
+++ Newton_Windows_Forms_Application1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using info.lundin.math;
+using System.Text.RegularExpressions;
+namespace Newton_Windows_Forms_Application1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            string inicial;
+            string expresion;
+            string derivada;
+            int iteraciones;
+            double tolerancia ;
+
+            try
+            {
+                inicial = this.textBox1.Text;
+                expresion = this.textBox2.Text;
+                derivada = this.textBox3.Text;
+                iteraciones = Convert.ToInt32(this.textBox5.Text);
+                tolerancia = Convert.ToDouble(this.textBox6.Text);                
+            }
+            catch (FormatException)
+            {
+                System.Windows.Forms.MessageBox.Show(this, "Format Error", "Advertencia", MessageBoxButtons.OK);
+                inicial = "0.0";
+                expresion = "x^2";
+                derivada = "2x";
+                iteraciones = 0;
+                tolerancia = 0;
+            }
+            ExpressionParser parse = new ExpressionParser();
+            double valor_inicial = parse.Parse(inicial);
+
+            Newton newton= new Newton();
+            newton.set_punto_inicio(Math.Round(valor_inicial,7));
+
+            string expresion1 = Regex.Replace(expresion,"e","Euler");
+            string expresion2 = Regex.Replace(derivada,"e","Euler");
+            newton.set_expresion(expresion1);
+            newton.set_derivada(expresion2);
+            newton.set_iteraciones(iteraciones);
+            newton.set_tolerancia(tolerancia);
+
+            double resultado = newton.Solucion(this.dataGridView1);
+            this.textBox4.Text = resultado.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Clear();
+        }
+    }
+}
---
+++ Newton_Windows_Forms_Application1/Newton.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using info.lundin.math;
+
+namespace Newton_Windows_Forms_Application1
+{
+    class Newton
+    {
+        private double inicio;
+        private string expresion;
+        private string derivada;
+        private int iteraciones;
+        private double tolerancia;
+        public Newton()
+        {
+
+        }
+        public Newton(double inicio,string expresion,string derivada,int iteraciones,double tolerancia)
+        {
+            this.inicio = inicio;
+            this.expresion = expresion;
+            this.derivada = derivada;
+            this.iteraciones = iteraciones;
+            this.tolerancia = tolerancia;
+        }
+
+        public void set_punto_inicio(double inicio)
+        {
+            this.inicio = inicio;
+        }
+        public void set_expresion(string expresion)
+        {
+            this.expresion = expresion;
+        }
+        public void set_derivada(string derivada)
+        {
+            this.derivada = derivada;
+        }
+        public void set_iteraciones(int iteraciones)
+        {
+            this.iteraciones = iteraciones;
+        }
+        public void set_tolerancia(double tolerancia)
+        {
+            this.tolerancia = tolerancia;
+        }
+
+        public double Solucion(System.Windows.Forms.DataGridView data)
+        {
+            double x=inicio;
+            int i = 1;
+            double fx;
+            double dfx;
+            double ax;
+            
+            do
+            {
+                fx = fun(x);
+                dfx = Dfun(x);
+                ax = x;
+                x = Math.Round(x - (fx / dfx),7);
+                data.Rows.Add(i, ax, fx, dfx, x);
+                i++;
+            } while (Math.Abs(fx) > this.tolerancia && i < this.iteraciones);
+            return x;
+        }
+
+        private double fun(double x)
+        {
+            ExpressionParser evaluador = new ExpressionParser();
+            evaluador.Values.Add("x",x);
+            double resultado = evaluador.Parse(this.expresion);
+            return resultado;
+        }
+
+        private double Dfun(double x)
+        {
+            ExpressionParser evaluador = new ExpressionParser();
+            evaluador.Values.Add("x", x);
+            double resultado = evaluador.Parse(this.derivada);
+            return resultado;
+        }
+
+    }
+}
